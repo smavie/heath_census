d5925ae8dec70affe1a31a1f1a32be2a8d658324
Sergio Mavie <sergio.mavie@gmail.com>
2025-01-07 11:53:39 +0200

    Search funtion created and contact form created

--- health_analysis.js
+++ health_analysis.js
@@ -30,6 +30,40 @@ function resetForm() {
     document.getElementById("condition").value = "";
   }
 
+  //Create a function for search request
+  function searchCondition() {
+    const input = document.getElementById('conditionInput').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+
+    fetch('health_analysis.json')
+      .then(response => response.json())
+      .then(data => {
+        const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+
+        if (condition) {
+          const symptoms = condition.symptoms.join(', ');
+          const prevention = condition.prevention.join(', ');
+          const treatment = condition.treatment;
+
+          resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
+          resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+
+          resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
+          resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
+          resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+        } else {
+          resultDiv.innerHTML = 'Condition not found.';
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        resultDiv.innerHTML = 'An error occurred while fetching data.';
+        console.log(response);
+      });
+  }
+    btnSearch.addEventListener('click', searchCondition);
+
 //Create the function that generates the report
 function generateReport() {
     const numPatients = patients.length;
